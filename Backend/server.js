d262ac4f0b560eba6814651529dd5868d4e624e4
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-07-07 18:57:59 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import { handleChatQuery } from './ai/canteenChatbot.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = 'zappadu_super_secret_jwt_key_123!';
-
+     
 // Middlewares
 app.use(cors({
   origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080'],
